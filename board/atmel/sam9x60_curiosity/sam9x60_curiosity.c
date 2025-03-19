d3bf2730eead5b2c11c15f8d02c748d5b37f1ccc
Lee Chang-Goo <105254953+cglee-lowa@users.noreply.github.com>
2025-03-19 17:47:44 +0900

    입력으로 사용할 GPIO2,3,4는 Pull-Up 설정을 유지하기 위해 IN 유지

--- board/atmel/sam9x60_curiosity/sam9x60_curiosity.c
+++ board/atmel/sam9x60_curiosity/sam9x60_curiosity.c
@@ -242,9 +242,9 @@ int board_init(void)
 	at91_set_pio_input(AT91_PIO_PORTB, 14, 1);
 	at91_set_pio_output(AT91_PIO_PORTA, 25, 0);
 	at91_set_pio_output(AT91_PIO_PORTD, 18, 0);
-	at91_set_pio_output(AT91_PIO_PORTD, 20, 0);
-	at91_set_pio_output(AT91_PIO_PORTD, 17, 0);
-	at91_set_pio_output(AT91_PIO_PORTD, 19, 0);
+	//at91_set_pio_output(AT91_PIO_PORTD, 20, 0);
+	//at91_set_pio_output(AT91_PIO_PORTD, 17, 0);
+	//at91_set_pio_output(AT91_PIO_PORTD, 19, 0);
 	at91_set_pio_output(AT91_PIO_PORTB, 11, 0);
 	at91_set_pio_output(AT91_PIO_PORTA, 29, 0);
 	at91_set_pio_output(AT91_PIO_PORTB, 14, 0);
