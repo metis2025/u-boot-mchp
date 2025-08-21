3efd7c4201bc0d017e0a46fb6fb9cfa02484ca39
Lee Chang-Goo <105254953+cglee-lowa@users.noreply.github.com>
2025-08-21 09:46:04 +0900

    PA25는 bootstrap에서 LOW로 설정

--- board/atmel/sam9x60_curiosity/sam9x60_curiosity.c
+++ board/atmel/sam9x60_curiosity/sam9x60_curiosity.c
@@ -231,7 +231,7 @@ int board_init(void)
 	at91_pio3_set_pio_pulldown(AT91_PIO_PORTD,  17, 0);
 	at91_pio3_set_pio_pulldown(AT91_PIO_PORTD,  18, 0);
 
-	at91_set_pio_input(AT91_PIO_PORTA, 25, 1);
+	// at91_set_pio_input(AT91_PIO_PORTA, 25, 1); // PA25는 BootStrap에서 LOW로 설정
 	at91_set_pio_input(AT91_PIO_PORTD, 18, 1);
 	at91_set_pio_input(AT91_PIO_PORTD, 20, 1);
 	at91_set_pio_input(AT91_PIO_PORTD, 17, 1);
@@ -240,7 +240,7 @@ int board_init(void)
 	at91_set_pio_input(AT91_PIO_PORTA, 29, 1);
 	at91_set_pio_input(AT91_PIO_PORTB, 14, 1);
  
-	at91_set_pio_output(AT91_PIO_PORTA, 25, 0);
+	// at91_set_pio_output(AT91_PIO_PORTA, 25, 0); // PA25는 BootStrap에서 LOW로 설정
 	at91_set_pio_output(AT91_PIO_PORTD, 18, 1); // Wi-Fi default Off
 	at91_set_pio_output(AT91_PIO_PORTD, 20, 0);
 	at91_set_pio_output(AT91_PIO_PORTD, 17, 0); // LTE OFF TEST -> ON
