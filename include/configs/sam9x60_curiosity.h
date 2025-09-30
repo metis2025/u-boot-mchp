558a2cacde00cc510eaa7e9b2639f302c254332a
Lee Chang-Goo <105254953+cglee-lowa@users.noreply.github.com>
2025-09-30 11:25:48 +0900

    128MB DDR TEST

--- include/configs/sam9x60_curiosity.h
+++ include/configs/sam9x60_curiosity.h
@@ -18,7 +18,7 @@
 
 /* SDRAM */
 #define CFG_SYS_SDRAM_BASE		0x20000000
-#define CFG_SYS_SDRAM_SIZE		0x4000000	/* 64 MB */
-// #define CFG_SYS_SDRAM_SIZE		0x8000000	/* 128 MB */
+// #define CFG_SYS_SDRAM_SIZE		0x4000000	/* 64 MB */
+#define CFG_SYS_SDRAM_SIZE		0x8000000	/* 128 MB */
 
 #endif
